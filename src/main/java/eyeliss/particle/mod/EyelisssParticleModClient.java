9150fa67cf98bb9566b277cae6fe4ed0af1f168a
Callie Talley <leoncatten@gmail.com>
2026-05-27 03:15:30 -0700

    Initial commit

---
+++ src/main/java/eyeliss/particle/mod/EyelisssParticleModClient.java
@@ -0,0 +1,10 @@
+package eyeliss.particle.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EyelisssParticleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/eyeliss/particle/mod/EyelisssParticleModDataGenerator.java
@@ -0,0 +1,11 @@
+package eyeliss.particle.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EyelisssParticleModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
