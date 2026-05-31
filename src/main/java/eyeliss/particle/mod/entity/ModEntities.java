5dde7b68a997fb95e2cf3f4db51c35164b9150ef
Callie Talley <leoncatten@gmail.com>
2026-05-31 12:27:49 -0700

    Removed an unused import statement.

--- src/main/java/eyeliss/particle/mod/entity/ModEntities.java
+++ src/main/java/eyeliss/particle/mod/entity/ModEntities.java
@@ -4,7 +4,6 @@ import eyeliss.particle.mod.EyelisssParticleMod;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
